343ac220659ebdd0a0581b8cdf84499508005ebe
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-02-27 14:48:42 -0500

    add title and image to home.html

--- src/main/java/com/detroitlabs/pizzapedia/controller/PizzaController.java
+++ src/main/java/com/detroitlabs/pizzapedia/controller/PizzaController.java
@@ -1,5 +1,6 @@
 package com.detroitlabs.pizzapedia.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PizzaController {
 
+    @Autowired
+    PizzaController pizzaController;
+
     @RequestMapping("/")
-    @ResponseBody
     public String displayHome(){
         String hello = "Hello";
         return hello;
--- src/main/java/com/detroitlabs/pizzapedia/data/PizzaRepository.java
+++ src/main/java/com/detroitlabs/pizzapedia/data/PizzaRepository.java
@@ -1,4 +1,7 @@
 package com.detroitlabs.pizzapedia.data;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PizzaRepository {
 }
